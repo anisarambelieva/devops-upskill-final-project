e6434324f088276a0ca18cec12f96689073d24ae
Anna Sarambelieva <ansa@milestone.dk>
2024-01-09 17:50:04 +0200

    Delete unnecessary iam role

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -1,8 +1,3 @@
-# source - github
-# plan
-# manual approval 
-# apply
-
 resource "aws_codepipeline" "codepipeline" {
   name     = "provision-infrastructure-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
@@ -69,59 +64,8 @@ resource "aws_codepipeline" "codepipeline" {
         configuration = {
             ProjectName = aws_codebuild_project.codebuild_project_apply_stage.name
         }
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
     }
-
-    actions = ["sts:AssumeRole"]
   }
-}
-
-resource "aws_iam_role" "codepipeline_role" {
-  name               = "codepipeline-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "codepipeline_policy" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "s3:PutObjectAcl",
-      "s3:PutObject",
-    ]
-
-    resources = [
-      aws_s3_bucket.codepipeline_bucket.arn,
-      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "codepipeline_policy" {
-  name   = "codepipeline_policy"
-  role   = aws_iam_role.codepipeline_role.id
-  policy = data.aws_iam_policy_document.codepipeline_policy.json
 }
 
 data "aws_kms_alias" "s3kmskey" {
