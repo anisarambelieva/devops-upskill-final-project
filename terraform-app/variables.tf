6e069d1fd12519d6de8cfaaebfb9072fdecb0a21
Anna Sarambelieva <ansa@milestone.dk>
2024-01-19 11:14:42 +0200

    Add backend config for terraform-app

--- terraform-app/variables.tf
+++ terraform-app/variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  type        = string
+  description = "The region in which to provision the resources"
+  default     = "eu-west-1"
+}
+
 variable "cluster_name" {
   type        = string
   description = "EKS cluster name"
