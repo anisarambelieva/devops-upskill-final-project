086493776a8c5bba666901f4a096bc45ed797efb
Anna Sarambelieva <ansa@milestone.dk>
2024-01-21 11:13:28 +0200

    Add private subnet

--- terraform-app/subnet.tf
+++ terraform-app/subnet.tf
@@ -7,3 +7,10 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.default.id
   map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "private" {
+  count             = var.az_count
+  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.default.id
+}
