c5393ba92aaf6f6ec9681291e096b92b60555412
Anna Sarambelieva <ansa@milestone.dk>
2024-01-21 21:12:24 +0200

    Remove env from app py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request
 import boto3
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 app = Flask(__name__, static_url_path='/static')
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-region_name = os.getenv('AWS_REGION')
-table_name = os.getenv('DYNAMODB_TABLE_NAME')
+dynamodb = boto3.resource('dynamodb', region_name="eu-west-1")
 
-dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key,
-                          region_name=region_name)
-
-table = dynamodb.Table(table_name)
+table = dynamodb.Table("newsletter-subscriptions")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,4 +18,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
