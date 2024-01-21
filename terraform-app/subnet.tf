e852817d35741ceb64e548f1198c54571321a7d3
Anna Sarambelieva <ansa@milestone.dk>
2024-01-21 18:49:35 +0200

    Add route tables

--- terraform-app/subnet.tf
+++ terraform-app/subnet.tf
@@ -1,5 +1,7 @@
 data "aws_availability_zones" "available" {}
 
+# PUBLIC
+
 resource "aws_subnet" "public" {
   count                   = var.az_count
   cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, var.az_count + count.index)
@@ -8,9 +10,62 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_main_route_table_association" "public_main" {
+  vpc_id         = aws_vpc.default.id
+  route_table_id = aws_route_table.public.id
+}
+
+# PRIVATE
+ 
 resource "aws_subnet" "private" {
   count             = var.az_count
   cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = aws_vpc.default.id
+}
+
+resource "aws_route_table" "private" {
+  count  = var.az_count
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
+  }
+}
+
+resource "aws_eip" "nat_gateway" {
+  count = var.az_count
+  vpc   = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  count         = var.az_count
+  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat_gateway[count.index].id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.az_count
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
