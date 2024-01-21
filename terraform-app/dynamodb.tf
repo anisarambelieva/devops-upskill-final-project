8d3743ac0fdadac85ff8bcf3ea32ecee9e9ace3b
Anna Sarambelieva <ansa@milestone.dk>
2024-01-21 10:13:59 +0200

    Revert dynamodb changes

--- terraform-app/dynamodb.tf
+++ terraform-app/dynamodb.tf
@@ -1,4 +1,6 @@
 resource "aws_dynamodb_table" "newsletter-subcriptions-table" {
+  name           = "newsletter-subscriptions"
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Email"
 
   attribute {
