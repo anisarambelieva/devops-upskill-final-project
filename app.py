da20bb876d80e1ebbe96d1b57f0f00c713b4783b
Anna Sarambelieva <ansa@milestone.dk>
2024-01-24 21:21:48 +0200

    Add test pipeline changes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ dynamodb = boto3.resource('dynamodb', region_name="eu-west-1")
 
 table = dynamodb.Table("newsletter-subscriptions")
 
+# TEST
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
