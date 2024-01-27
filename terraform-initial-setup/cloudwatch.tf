3c4546e998ca98c75bfd565cce36c2a319b634d7
Anna Sarambelieva <ansa@milestone.dk>
2024-01-27 13:45:11 +0200

    Add alarms for build and apply

--- terraform-initial-setup/cloudwatch.tf
+++ terraform-initial-setup/cloudwatch.tf
@@ -1,3 +1,21 @@
+resource "aws_cloudwatch_metric_alarm" "codebuild_build_failure_alarm" {
+  alarm_name          = "CodeBuildBuildFailureAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FailedBuilds"
+  namespace           = "AWS/CodeBuild"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "Alarm for CodeBuild Terraform plan failure"
+
+  dimensions = {
+    ProjectName = var.codebuild_build_project_name
+  }
+
+  alarm_actions = [aws_sns_topic.codebuild_notifications.arn]
+}
+
 resource "aws_cloudwatch_metric_alarm" "codebuild_plan_failure_alarm" {
   alarm_name          = "CodeBuildPlanFailureAlarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -10,7 +28,25 @@ resource "aws_cloudwatch_metric_alarm" "codebuild_plan_failure_alarm" {
   alarm_description   = "Alarm for CodeBuild Terraform plan failure"
 
   dimensions = {
-    ProjectName = "terraform-plan-codebuild-project"
+    ProjectName = var.codebuild_plan_project_name
+  }
+
+  alarm_actions = [aws_sns_topic.codebuild_notifications.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "codebuild_apply_failure_alarm" {
+  alarm_name          = "CodeBuildApplyFailureAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FailedBuilds"
+  namespace           = "AWS/CodeBuild"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "Alarm for CodeBuild Terraform apply failure"
+
+  dimensions = {
+    ProjectName = var.codebuild_apply_project_name
   }
 
   alarm_actions = [aws_sns_topic.codebuild_notifications.arn]
